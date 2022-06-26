ba96e415839518ccd492452efd621b0cc476d775
ecaoyuk <yukun.cao@ericsson.com>
2022-06-26 22:56:59 +0200

    create per process kernel PTL

--- kernel/defs.h
+++ kernel/defs.h
@@ -160,8 +160,11 @@ int             uartgetc(void);
 // vm.c
 void            kvminit(void);
 void            kvminithart(void);
-uint64          kvmpa(uint64);
-void            kvmmap(uint64, uint64, uint64, int);
+pagetable_t     kvminit_newpgtbl(void);
+// uint64          kvmpa(uint64);
+// void            kvmmap(uint64, uint64, uint64, int);
+uint64          kvmpa(pagetable_t, uint64);
+void            kvmmap(pagetable_t, uint64, uint64, uint64, int);
 int             mappages(pagetable_t, uint64, uint64, uint64, int);
 pagetable_t     uvmcreate(void);
 void            uvminit(pagetable_t, uchar *, uint);
@@ -178,7 +181,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-void             vmprint(pagetable_t pagetable); // 添加函数声明
+void            vmprint(pagetable_t pagetable); // 添加函数声明
+void            kvm_free_kernelpgtbl(pagetable_t pagetable);
 
 // plic.c
 void            plicinit(void);
